aa7cf304f59d61603d1434dc5fdffd49bfaa3f30
hasanali112 <sohagali.ru.ac@gmail.com>
2025-03-23 22:22:40 +0600

    create tab list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,48 +15,88 @@ const tabList = [
     label: "Navigation",
   },
   {
-    value: "password",
-    label: "Password",
+    value: "banner",
+    label: "Banner",
+  },
+  {
+    value: "features",
+    label: "Features",
+  },
+  {
+    value: "forms",
+    label: "Forms",
   },
 ];
 
 const Home = () => {
   return (
     <div className="w-full  max-w-[1540px] mx-auto  px-[20px] h-screen mt-5">
-      <Tabs defaultValue="account" className="w-[400px]">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
-        </TabsList>
-        <TabsContent value="account">
-          <Card>
-            <CardHeader>
-              <CardTitle>Account</CardTitle>
-              <CardDescription></CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div></div>
-            </CardContent>
-            <CardFooter>
-              <Button>Save changes</Button>
-            </CardFooter>
-          </Card>
-        </TabsContent>
-        <TabsContent value="password">
-          <Card>
-            <CardHeader>
-              <CardTitle>Password</CardTitle>
-              <CardDescription></CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div></div>
-            </CardContent>
-            <CardFooter>
-              <Button>Save password</Button>
-            </CardFooter>
-          </Card>
-        </TabsContent>
-      </Tabs>
+      <div className="flex gap-10 items-center">
+        <div>
+          <Tabs defaultValue="account" className="w-[500px] ">
+            <TabsList className="grid w-full grid-cols-5 cursor-pointer">
+              {tabList.map((tab) => (
+                <TabsTrigger
+                  key={tab.value}
+                  value={tab.value}
+                  className="cursor-pointer"
+                >
+                  {tab.label}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+
+            {/* navigation */}
+            <TabsContent value="navigation">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Account</CardTitle>
+                  <CardDescription></CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <div></div>
+                </CardContent>
+                <CardFooter>
+                  <Button>Save changes</Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+
+            {/* banner */}
+            <TabsContent value="banner">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Password</CardTitle>
+                  <CardDescription></CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <div></div>
+                </CardContent>
+                <CardFooter>
+                  <Button>Save password</Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+
+            {/* banner */}
+            <TabsContent value="features">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Password</CardTitle>
+                  <CardDescription></CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <div></div>
+                </CardContent>
+                <CardFooter>
+                  <Button>Save password</Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+          </Tabs>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
